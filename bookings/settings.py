19e2e8e685b58ab21a9cb16299f73adf02c7ab05
Edouard102 <>
2023-12-22 17:46:47 +0000

    create secret key

--- bookings/settings.py
+++ bookings/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rma(z#%)kx04wq%mtxuuj*0kaauvr^11!hn0rg^0f^7j&&w=r4'
+SECRET_KEY = os.environ.get("top^new@12clef")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
